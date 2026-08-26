85948b70b95e51038aa008066a36cc4bd5781a3d
lumer09 <ludy09reyna@gmail.com>
2026-08-25 19:37:20 -0500

    fix: resolve workspace conflicts, configure Next.js turbopack, and fix all linting warnings

--- Gold_back/eslint.config.js
+++ Gold_back/eslint.config.js
@@ -2,6 +2,9 @@ module.exports = [
   { ignores: ['node_modules/', 'dist/', '*.config.js'] },
   {
     languageOptions: { ecmaVersion: 2022, sourceType: 'commonjs' },
-    rules: { 'no-unused-vars': 'warn', 'no-console': 'off' }
+    rules: { 
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }], 
+      'no-console': 'off' 
+    }
   }
 ];
