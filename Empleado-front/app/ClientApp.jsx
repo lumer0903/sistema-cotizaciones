dc988b8bec6e9c935c8c51425a2149d6651c89cd
lumer09 <ludy09reyna@gmail.com>
2026-09-15 15:46:23 -0500

    chore: eliminar carpetas obsoletas

--- Empleado-front/app/ClientApp.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { BrowserRouter } from 'react-router-dom';
-import { App } from '../src/presentation/App';
-
-export default function ClientApp() {
-  return <BrowserRouter><App /></BrowserRouter>;
-}
--- Empleado-front/app/[...slug]/page.jsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const ClientApp = dynamic(() => import('../ClientApp'), { ssr: false });
-
-export default function CatchAllPage() { return <ClientApp />; }
